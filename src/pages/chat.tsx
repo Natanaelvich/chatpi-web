dbe2e4a9f578895b5b9eeb3e24bc354db2dff0ce
Natanael Lima <taelima1997@gmail.com>
2020-11-22 20:47:00 -0300

    created connection bettewen users

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,11 +1,13 @@
 import { useAuth } from '@/hooks/modules/AuthContext';
 import { Chat, Messages } from '@/styles/Chat/styles';
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import io from 'socket.io-client';
 import { Container } from '../styles/SingnIn/styles';
 
 export default function ChatHome() {
   const { user } = useAuth();
+
+  const [messages, setMessages] = useState([]);
 
   const socket = useMemo(() => {
     return io(process.env.NEXT_PUBLIC_API_KEY, {
@@ -13,12 +15,22 @@ export default function ChatHome() {
     });
   }, [user]);
 
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(oldMessages => [...oldMessages, message]);
+    });
+  }, [socket]);
+
   return (
     <Container>
       <Chat>
         <input />
 
-        <Messages />
+        <Messages>
+          {messages.map(m => (
+            <p>{m}</p>
+          ))}
+        </Messages>
 
         <input />
 
