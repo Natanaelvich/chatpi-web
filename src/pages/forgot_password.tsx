5525137495f8367927f4c257ad225c02cf36fda8
Natanael Lima <taelima1997@gmail.com>
2020-12-02 09:50:09 -0300

    added page forgot password

---
+++ src/pages/forgot_password.tsx
@@ -0,0 +1,114 @@
+import React, { useCallback, useRef, useState } from 'react';
+import * as Yup from 'yup';
+
+import { FiMail, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import Image from 'next/image';
+import {
+  Logo,
+  BackGround,
+  Container,
+  Content,
+  AnimationContainer,
+} from '@/styles/pages/ForgotPassword';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import getValidationErros from '../utils/getValidationErros';
+import api from '../services/api';
+import { useToast } from '../hooks/modules/ToastContext';
+
+const ForgotPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const router = useRouter();
+  const { addToast } = useToast();
+
+  const [loading, setLoading] = useState(0);
+
+  const hanleSingnUp = useCallback(
+    async (data: { email: string }) => {
+      try {
+        setLoading(1);
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail valido.'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        const { email } = data;
+
+        await api.post('/password/forgot', {
+          email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'E-mail de recuperação enviado.',
+          description:
+            'Enviamos um e-amil para cofirmar a recuperação de senha.',
+        });
+
+        router.push('/');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErros(error);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao enviar E-mail!',
+          description: 'Verifique os dados e tente novamente.',
+        });
+      } finally {
+        setLoading(0);
+      }
+    },
+    [addToast, router],
+  );
+
+  return (
+    <Container>
+      <BackGround>
+        <Image src="/Logo.png" alt="Chat PI" width={620} height={600} />
+      </BackGround>
+      <Content>
+        <AnimationContainer>
+          <Logo>
+            <Image src="/Logo.png" alt="Chat PI" width={165} height={160} />
+          </Logo>
+
+          <Form ref={formRef} onSubmit={hanleSingnUp}>
+            <h1>Recuperar senha</h1>
+
+            <Input placeholder="E-mail" name="email" icon={FiMail} />
+
+            <Button loading={loading} type="submit">
+              Recuperar
+            </Button>
+          </Form>
+          <Link href="/">
+            <a>
+              <FiArrowLeft />
+              Voltar para logon
+            </a>
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <BackGround>
+        <Image src="/Logo.png" alt="Chat PI" width={620} height={600} />
+      </BackGround>
+    </Container>
+  );
+};
+
+export default ForgotPassword;
---
+++ src/styles/pages/ForgotPassword.ts
@@ -0,0 +1,105 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+const appearFromRight = keyframes`
+from {
+  opacity : 0;
+  transform : translateX(50px);
+}
+to {
+  opacity : 1;
+  transform : translateX(0)
+}
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  place-content: center;
+
+  animation: ${appearFromRight} 1s;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+export const BackGround = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+
+  @media (max-width: 683px) {
+    & {
+      display: none;
+    }
+  }
+`;
+export const Logo = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (min-width: 683px) {
+    & {
+      display: none;
+    }
+  }
+`;
