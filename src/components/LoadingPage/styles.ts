2473ed3f2f21e158bfa6caba7b80693492ccb542
Natanael Lima <taelima1997@gmail.com>
2021-02-07 12:02:45 -0300

    create loading page

---
+++ src/components/LoadingPage/styles.ts
@@ -0,0 +1,52 @@
+import styled, { css } from 'styled-components';
+
+const animationLogo = css`
+  @keyframes heartbeat {
+    from {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      -webkit-transform-origin: center center;
+      transform-origin: center center;
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+    10% {
+      -webkit-transform: scale(0.91);
+      transform: scale(0.91);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+    }
+    17% {
+      -webkit-transform: scale(0.98);
+      transform: scale(0.98);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+    33% {
+      -webkit-transform: scale(0.87);
+      transform: scale(0.87);
+      -webkit-animation-timing-function: ease-in;
+      animation-timing-function: ease-in;
+    }
+    45% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+    }
+  }
+  animation: heartbeat 1.5s ease-in-out infinite both;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(270deg, #3f3a79 27.66%, #d56065 98.55%);
+`;
+
+export const Logo = styled.div`
+  ${animationLogo}
+`;
