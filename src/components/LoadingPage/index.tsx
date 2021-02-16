2b550fc64185a9a0306075afb2f1323d49ed5429
Natanael Lima <taelima1997@gmail.com>
2021-02-15 22:18:49 -0300

    remove seo component

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
-import SEO from '../Seo';
 
 import { Container, Logo } from './styles';
 
 const LoadingPage: React.FC = () => {
   return (
     <Container>
-      <SEO title="ChatPI" shouldIndexPage={false} />
       <Logo>
         <Image src="/Logo.png" alt="Chat PI" width={165} height={160} />
       </Logo>
