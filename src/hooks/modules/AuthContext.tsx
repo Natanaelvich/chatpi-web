a23d9415bcf1a1bd8149b820e923175133f3a8e3
Natanael Lima <taelima1997@gmail.com>
2021-03-25 11:42:48 -0300

    add logic auth user

--- src/hooks/modules/AuthContext.tsx
+++ src/hooks/modules/AuthContext.tsx
@@ -1,5 +1,11 @@
 import { useRouter } from 'next/router';
-import React, { createContext, useCallback, useState, useContext } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useState,
+  useContext,
+  useEffect,
+} from 'react';
 import Cookies from 'js-cookie';
 import api from '../../services/api';
 
@@ -30,17 +36,23 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({ children }) => {
   const router = useRouter();
 
-  const [data, setData] = useState<AuthState>(() => {
-    const token = Cookies.get('GoBarbertoken');
-    const user = Cookies.get('Gobarberuser');
+  const [data, setData] = useState<AuthState>({} as AuthState);
 
-    if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
-      return { token, user: JSON.parse(user) };
+  useEffect(() => {
+    function verifyUserData() {
+      const token = Cookies.get('GoBarbertoken');
+      const user = Cookies.get('Gobarberuser');
+      if (token && user) {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+        setData({ token, user: JSON.parse(user) });
+        return;
+      }
+
+      router.replace('/');
     }
 
-    return {} as AuthState;
-  });
+    verifyUserData();
+  }, [router]);
 
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
