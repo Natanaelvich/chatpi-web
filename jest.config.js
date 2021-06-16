f3d2ce500dcb61cea5d6af4d47a17e728ac74db8
Natanael Lima <taelima1997@gmail.com>
2021-06-16 15:48:40 -0300

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  "moduleNameMapper": {
+    "@/components/(.*)": "<rootDir>/src/components/$1",
+    "@/pages/(.*)": "<rootDir>/src/pages/$1"
+  },
+}
