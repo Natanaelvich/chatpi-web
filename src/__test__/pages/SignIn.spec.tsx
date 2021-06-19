c0b595f694f8861df42ae40b07159056c11f6ced
Natanael Lima <taelima1997@gmail.com>
2021-06-18 23:35:08 -0300

    remove imports unused

--- src/__test__/pages/SignIn.spec.tsx
+++ src/__test__/pages/SignIn.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, waitFor, screen } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { useRouter } from 'next/router';
 import SingnIn from '../../pages';
 
