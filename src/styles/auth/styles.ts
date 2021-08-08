4812a0e85b881d6b56ef4414ef1e84b6b0f9471d
Natanael Lima <taelima1997@gmail.com>
2021-08-08 11:28:41 -0300

    update styles with aith styles

--- src/styles/auth/styles.ts
+++ src/styles/auth/styles.ts
@@ -1,45 +1,55 @@
 import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
+export const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 export const Container = styled.div`
+  height: 100vh;
   display: flex;
   flex: 1;
   overflow: auto;
-  height: 100vh;
   background: linear-gradient(270deg, #3f3a79 27.66%, #d56065 98.55%);
 `;
+export const Content = styled.div`
+  display: flex;
 
-const appearFromRight = keyframes`
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 1020px;
+  margin: auto;
+`;
+const appearFromLeft = keyframes`
 from {
   opacity : 0;
-  transform : translateX(50px);
+  transform : translateX(-50px);
 }
 to {
   opacity : 1;
   transform : translateX(0)
 }
 `;
-
 export const AnimationContainer = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
-
   align-items: center;
   place-content: center;
-
-  animation: ${appearFromRight} 1s;
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
     width: 340px;
     text-align: center;
-
     h1 {
       font-size: 41px;
       margin-bottom: 24px;
       font-family: 'Redressed', serif;
     }
-
     a {
       color: #f4ede8;
       display: block;
@@ -51,18 +61,19 @@ export const AnimationContainer = styled.div`
         color: ${shade(0.2, '#f4ede8')};
       }
     }
-    display: flex;
-    flex-direction: column;
-    label {
-      font-size: 21px;
-      margin-right: 12px;
-      align-self: flex-start;
-      margin-top: 12px;
-      input {
-        margin-left: 12px;
+
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+
+      label {
+        font-size: 21px;
+        input {
+          margin-left: 12px;
+        }
       }
     }
-
     select {
       margin-top: 12px;
       width: 100%;
@@ -82,14 +93,14 @@ export const AnimationContainer = styled.div`
   }
 
   > a {
-    color: #f4ede8;
+    color: #ffff;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#fff')};
     }
 
     svg {
@@ -97,14 +108,29 @@ export const AnimationContainer = styled.div`
     }
   }
 `;
-
-export const Content = styled.div`
+export const Background = styled.div`
   display: flex;
-  flex-direction: column;
-
+  flex: 1;
   align-items: center;
-  place-content: center;
+  justify-content: center;
+  height: 100vh;
 
-  width: 100%;
-  max-width: 700px;
+  @media (max-width: 768px) {
+    & {
+      display: none;
+    }
+  }
+`;
+export const Logo = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  display: none;
+
+  @media (max-width: 768px) {
+    & {
+      display: block;
+    }
+  }
 `;
