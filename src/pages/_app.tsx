ca0a8977c9f19b2f68bcd44b67c6d5490afefe9b
Natanael Lima <taelima1997@gmail.com>
2021-09-02 23:44:25 -0300

    add component NextNprogress to  main app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import AppProvider from '@/hooks';
 import Globalstyles from '@/styles/Globalstyles';
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
       <Globalstyles />
       <Component {...pageProps} />
+      <NextNprogress
+        color="#f231a5"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
     </AppProvider>
   );
 }
